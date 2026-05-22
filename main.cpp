be84ab7dcc42f20eff3c729778b6e0ed6e9c1e4f
CraigSau <craigwsauers@gmail.com>
2026-05-22 14:21:30 -0500

    Initial work.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <ostream>
 
 int main() {
+  // Image
+  int image_width = 256;
+  int image_height = 256;
 
+  // Render
+  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+  for (int i = 0; i < image_height; i++) {
+    std::clog << "\rScanlines remaining: " << (image_height - i) << ' '
+              << std::flush;
+    for (int j = 0; j < image_width; j++) {
+      auto r = double(j) / (image_width - 1);
+      auto g = double(i) / (image_height - 1);
+      auto b = 0.0;
+
+      int ir = int(255.999 * r);
+      int ig = int(255.999 * g);
+      int ib = int(255.999 * b);
+
+      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+    }
+  }
+
+  std::clog << "\rDone. \n";
 }
